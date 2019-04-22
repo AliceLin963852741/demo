5866e3279b09558c1b519e1fc261cde5947aad6b
alicelin963852741 <alicelin963852741@gmail.com>
2019-04-22 17:14:46 +0800

    move the ctrls

--- Script/ctrls/myCtrl.js
+++ Script/ctrls/myCtrl.js
@@ -1,17 +1,22 @@
-
-app.controller('myController',['$http',function($http){
-    console.log('111');
-  var self = this;
-  self.url='https://next.json-generator.com/api/json/get/EJuTb6mO8';
-  $http({
-    method:'GET',
-    url:self.url,
-  }).then(function(response){
-    self.data = response.data.InboxMailList;
-    //true
-    console.log(self.data)
-  },function(response){
-    //false
-    console.log("error");
-  });
-}]);
+app.controller("myController", [
+  "$http",
+  function($http) {
+    console.log("111");
+    var self = this;
+    self.url = "https://next.json-generator.com/api/json/get/EJuTb6mO8";
+    $http({
+      method: "GET",
+      url: self.url
+    }).then(
+      function(response) {
+        self.data = response.data.InboxMailList;
+        //true
+        console.log(self.data);
+      },
+      function(response) {
+        //false
+        console.log("error");
+      }
+    );
+  }
+]);
