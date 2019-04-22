6199156f0aee40c795b438c55ccfe52fcb27e8e7
alicelin963852741 <alicelin963852741@gmail.com>
2019-04-22 15:06:38 +0800

    update1

---
+++ app.js
@@ -0,0 +1,17 @@
+var app = angular.module('myApp',[]);
+app.controller('myController',['$http',function($http){
+    console.log('111');
+  var self = this;
+  self.url='https://next.json-generator.com/api/json/get/EJuTb6mO8';
+  $http({
+    method:'GET',
+    url:self.url,
+  }).then(function(response){
+    self.data = response.data.InboxMailList;
+    //true
+    console.log(self.data)
+  },function(response){
+    //false
+    console.log("error");
+  });
+}]);
