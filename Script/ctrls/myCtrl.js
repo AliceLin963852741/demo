e138bec43453a11b01f69448698be24740e87745
alicelin963852741 <alicelin963852741@gmail.com>
2019-04-23 15:51:15 +0800

    update

--- Script/ctrls/myCtrl.js
+++ Script/ctrls/myCtrl.js
@@ -6,15 +6,15 @@ app.controller("myController", [
     // console.log(this,'the myCtrl this')
     var self= this;
     // console.log(self,'assign self to this')
-    // self.aa = 'aa'
+    self.aa = 'aa'
     // console.log(self,'assign another key&value to self')
     // console.log(self,'Now the myCtrl have nothing&aa')
     myService.getEvenInfo().then(
       function(response) {
-      // console.log(this,'the myService'sgetEvenInfo this')
+      // console.log(this,'the myService.sgetEvenInfo this');
         self.data = response.data.InboxMailList;//get the myCtrl this
         //true
-        console.log(self.data);
+        console.log(self.data,'self.data,這個就變成myCtrl裏的{}');
       },
       function(response) {
         //false
