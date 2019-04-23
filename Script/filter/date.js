6286ada565e35e2e9a50a28637637149b1204d67
alicelin963852741 <alicelin963852741@gmail.com>
2019-04-23 18:22:27 +0800

    update filter

--- Script/filter/date.js
+++ Script/filter/date.js
@@ -1,4 +1,10 @@
-app.controller("myController",function () {
+app.filter("data",function () {
+    console.log('123')
     var self = this;
     self.Dtime = new Date().getTime();
+    // return function (jsonDate) {
+    //     if (!jsonDate)
+    //         return "";
+    //     return moment(jsonDate).format('YYYY/MM/DD (dd) HH:mm:ss');
+    // };
 });
