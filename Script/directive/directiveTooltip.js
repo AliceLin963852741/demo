1f79c481833a5c7ce282d9505729750af836623c
alicelin963852741 <alicelin963852741@gmail.com>
2019-04-23 18:05:22 +0800

    update tooltip

--- Script/directive/directiveTooltip.js
+++ Script/directive/directiveTooltip.js
@@ -2,6 +2,7 @@
 app.directive('directiveTooltip', function(){
     return {
         restrict: 'E',
-        template:'<button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="left" title="{{data.Subject}}">{{data.Subject}}</button>'
+        // template:'<button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="left" title="{{data.Subject}}">{{data.Subject}}</button>'
+        template:'<button type="button" class="btn btn-default bgtn" >{{data.Subject}}<span class="bgtx">{{data.Subject}}</span></button>'
     }
 });
