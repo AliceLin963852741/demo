a5e31891da773736f2d85bf50fd3273cbd052d2b
alicelin963852741 <alicelin963852741@gmail.com>
2019-04-24 14:32:59 +0800

    directive done

--- Script/directive/directiveTooltip.js
+++ Script/directive/directiveTooltip.js
@@ -1,11 +1,12 @@
 
 app.directive('directiveTooltip', function(){
     return {
-        restrict: 'E',
-        template:'<button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="right" title="{{data.Subject}}">{{data.Subject}}</button>',
-        // <button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="right" title="i am new text">i am new buttom</button>
-        link:function(){
-            $('[data-toggle="tooltip"]').tooltip();
+        restrict: 'A',
+        link: function(scope,element,attrs){
+            $(element).tooltip({
+                title:attrs.settext,
+                placement:'right'
+            });
         }
         
     }
